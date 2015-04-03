6c9167baf9d54234df96c803b4c9cc0ea9206599
Woonsan Ko <w.ko@onehippo.com>
2015-04-02 23:07:06 -0400

    initial hippo project import

---
+++ site/src/main/java/org/example/customgallerypicker/demo/beans/BaseDocument.java
@@ -0,0 +1,9 @@
+package org.example.customgallerypicker.demo.beans;
+
+import org.hippoecm.hst.content.beans.Node;
+import org.hippoecm.hst.content.beans.standard.HippoDocument;
+
+@Node(jcrType="customgallerypickerdemo:basedocument")
+public class BaseDocument extends HippoDocument {
+
+}
